85c1b8e2f6c3bed3c1580a36080a55bf2b621d0e
Rain305 <30487503+BiaxialRain305@users.noreply.github.com>
2023-11-08 15:56:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,28 @@
-from datetime import datetime, timedelta
-from chessdotcom import client
-import os
-import datetime
+import requests
+import json
 
-indent = "\t\t\t\t\t\t"
-# Gets all the games played
-def all_games(username):
-    folder_name = f"{username} - Games"
-    timestamp = client.get_player_profile(username).player.joined
-    joined_datetime = datetime.datetime.fromtimestamp(timestamp)
-    current_datetime = datetime.datetime.now()
-    elapsed_time = current_datetime - joined_datetime
-    num_months = elapsed_time.days // 30
-    inp = input(f"{indent}Would you like each game in a seperate pgn file? (y/n): ")
-    while inp.lower() not in ["y", "n"]:
-        inp = input(f"{indent}Invalid Input\n{indent}Would you like each game in a seperate pgn file? (y/n): ").lower()
-    
-    os.mkdir(folder_name)
-    print(f"{indent}Can't see the files? Click the 'Show Code' button on the right side ------>")    
-    # Read list
-    for i in range(num_months+1):
-        month = (joined_datetime + timedelta(days=30*i)).strftime("%m")
-        year = (joined_datetime + timedelta(days=30*i)).strftime("%Y")
-        games_str = client.get_player_games_by_month_pgn(username, year, month).json["pgn"]["pgn"]
-        lines = games_str.split('\n\n')
-        result = []
-        current_line = ""
-        for i, line in enumerate(lines):
-            current_line += line
-            if (i+1) % 2 == 0:  # even line number (including 0)
-                result.append(current_line)
-                current_line = ""
-        
-        if current_line:
-            result.append(current_line)
-    if inp.lower() == "n":
-        file_name = os.path.join(folder_name, "All_Games.pgn")
-        f = open(file_name, "w")
-        f.write("\n".join(result))
-        f.close()
-    else:
-        for count, i in enumerate(result):
-            file_name = os.path.join(folder_name, f"Game{count}.pgn")
-            file = open(file_name, "w")
-            file.write(i)
-            file.close() 
+username = input("Enter Chess.com username: ")
+headers = {
+    'User-Agent': username
+}
 
-if __name__ == "__main__":
-    username = input(f"{indent}Enter a chess.com username: ")
-    all_games(username)
-    print("DONE")
+# Retrieve a list of archives
+all_data = []
+for url in requests.get(f"https://api.chess.com/pub/player/{username}/games/archives", headers=headers).json()[
+    "archives"]:
+    all_data.append(requests.get(url, headers=headers).json())
+
+# Retrieve games from archives
+games = []
+for row in all_data:
+    for game in row["games"]:
+        games.append(game)
+
+# Save games as JSON file
+with open("games2.json", "w") as file:
+    json.dump({"games": games}, file, indent=4)
+
+# Save PGN data
+with open("games.pgn", "w") as file:
+    for game in games:
+        file.write(game["pgn"] + "\n")
